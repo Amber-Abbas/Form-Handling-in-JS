52566ca2c5ca98ff3f34b3b0d37840edfdb341fa
Amber Abbas. <a.abbas@crecentech.com>
2024-10-09 08:09:39 -0500

    work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function set_error(id, error) {
   // Set errors inside the tag of the element with the given id
   var element = document.getElementById(id);
+  console.log(element, id);
   var errorElement = element.querySelector(".form-error");
+
   console.log("errorElement", errorElement);
   if (errorElement) {
     errorElement.innerHTML = error;
@@ -12,14 +14,26 @@ function set_error(id, error) {
 function validateForm(event) {
   event.preventDefault();
   let errors = [];
-  // Get the input elements by their ID
   var emailInput = document.getElementById("email");
+  var contInput = document.getElementById("phone");
+
+  if (contInput.value.length === 0) {
+    set_error("phone-box", "Phone number is required");
+  } else if (/^[0-9()\-+]*$/.test(contInput.value)) {
+    set_error("phone-box", ""); // Clear the error if it's valid
+  } else {
+    set_error("phone-box", "Phone number is invalid");
+  }
+
   let validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (emailInput.value.length < 3 || !emailInput.value.match(validRegex)) {
+  if (emailInput.value.length === 0) {
+    set_error("email-box", "Email should not be empty");
+  } else if (!emailInput.value.match(validRegex)) {
     set_error("email-box", "Invalid email address");
   } else {
-    set_error("email", "");
+    set_error("email-box", "");
   }
-  return true;
+
+  return true; // Or you can return false if validation fails
 }
