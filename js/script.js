1ff056c0266bd0294b53338291f777b415c10d04
Amber Abbas. <a.abbas@crecentech.com>
2024-10-10 07:19:54 -0500

    work

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 function set_error(id, error) {
-  // Set errors inside the tag of the element with the given id
   var element = document.getElementById(id);
-  console.log(element, id);
+  console.log("id", id);
   var errorElement = element.querySelector(".form-error");
-
-  console.log("errorElement", errorElement);
+  console.log("errorElement", errorElement, "id", id);
   if (errorElement) {
     errorElement.innerHTML = error;
     errorElement.style.color = "red";
@@ -16,6 +14,34 @@ function validateForm(event) {
   let errors = [];
   var emailInput = document.getElementById("email");
   var contInput = document.getElementById("phone");
+  var compNameInput = document.getElementById("comp-name");
+  var prefixInput = document.getElementById("prefix");
+  var startNoInput = document.getElementById("start-no");
+  var prefixInput = document.getElementById("prefix");
+  var codeLengthInput = document.getElementById("code-length");
+  if (prefixInput.value.length === 0) {
+    set_error("prefix-box", "prefix is required"); // Clear the error if it's valid
+  } else {
+    set_error("prefix-box", "");
+  }
+  if (startNoInput.value.length === 0) {
+    set_error("start-no-box", "start-no is required"); // Clear the error if it's valid
+  } else {
+    set_error("start-no-box", "");
+  }
+
+  if (codeLengthInput.value.length === 0) {
+    set_error("code-length-box", "code-lungth is required"); // Clear the error if it's valid
+  } else {
+    set_error("code-length-box", "");
+  }
+
+  if (compNameInput.value.length === 0) {
+    console.log(compNameInput.value.length);
+    set_error("comp-name-box", "Company Name is required"); // Clear the error if it's valid
+  } else {
+    set_error("comp-name-box", "");
+  }
 
   if (contInput.value.length === 0) {
     set_error("phone-box", "Phone number is required");
@@ -28,7 +54,7 @@ function validateForm(event) {
   let validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (emailInput.value.length === 0) {
-    set_error("email-box", "Email should not be empty");
+    set_error("email-box", "Email is required");
   } else if (!emailInput.value.match(validRegex)) {
     set_error("email-box", "Invalid email address");
   } else {
